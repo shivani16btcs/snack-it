dda5c1a1bba47f567e2645612dfab39435d59efd
shivani yadav <shivani.yadav01@nagarro.com>
2023-03-19 15:37:54 +0530

    stable

--- snack-it-frontend/src/app/app.module.ts
+++ snack-it-frontend/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../header/header.component';
 import { TaglineComponent } from 'src/tagline/tagline.component';
+// import { LoginComponent } from './auth/login/login.component';
+// import { RegisterComponent } from './auth/register/register.component';
+// import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TaglineComponent
+    TaglineComponent,
+    // LoginComponent,
+    // RegisterComponent,
+    // ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
