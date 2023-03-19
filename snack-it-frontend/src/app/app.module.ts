491739c283ff7e6a70f667dc945aeef196be5d49
shivani yadav <shivani.yadav01@nagarro.com>
2023-03-19 15:37:42 +0530

    stable

--- snack-it-frontend/src/app/app.module.ts
+++ snack-it-frontend/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from '../header/header.component';
+import { TaglineComponent } from 'src/tagline/tagline.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaglineComponent
   ],
   imports: [
     BrowserModule,
